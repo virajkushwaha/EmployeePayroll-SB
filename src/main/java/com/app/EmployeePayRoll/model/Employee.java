01714f717d311ec300a2290c9f4d730495c7dcd0
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-27 16:42:37 +0530

    Lombok us not working

--- src/main/java/com/app/EmployeePayRoll/model/Employee.java
+++ src/main/java/com/app/EmployeePayRoll/model/Employee.java
@@ -4,12 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "employee")
 public class Employee {
@@ -25,28 +24,28 @@ public class Employee {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public String getDepartment() {
+        return department;
     }
 
     public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
+    public String getName() {
+        return name;
     }
 
-    public String getDepartment() {
-        return department;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 
     public void setDepartment(String department) {

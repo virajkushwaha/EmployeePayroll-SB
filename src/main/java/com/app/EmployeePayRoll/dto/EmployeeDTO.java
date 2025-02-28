ba70aab0050b7fe5b8a57ae09591132908038022
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-28 17:48:35 +0530

    [Viraj] Add.Adding application-dev properties

--- src/main/java/com/app/EmployeePayRoll/dto/EmployeeDTO.java
+++ src/main/java/com/app/EmployeePayRoll/dto/EmployeeDTO.java
@@ -1,6 +1,8 @@
 package com.app.EmployeePayRoll.dto;
 
 import com.app.EmployeePayRoll.model.Employee;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.websocket.server.ServerEndpoint;
 import lombok.*;
 
@@ -11,31 +13,12 @@ import lombok.*;
 @Data
 @Setter
 public  class EmployeeDTO {
+
+    @NotBlank(message = "Name Cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")
     private String name ;
+
     private String department ;
     private double salary;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
